64013630c18be180b5e4e46122c4cf3e769caa40
bedrock-fire <60242683+bedrock-fire@users.noreply.github.com>
2021-02-18 15:54:10 -0600

    Update finnhub_1.py

--- Pull API/finnhub_1.py
+++ Pull API/finnhub_1.py
@@ -6,7 +6,7 @@ Created on Mon Feb 15 20:55:32 2021
 """
 # Code source:
 # https://finnhub.io/docs/api/introduction
-    
+
 import requests as req
 
 # For .json files:
@@ -23,11 +23,9 @@ print(aapl_ohlc)
 
 ## Candlestick Data (1yr):
 ## Also includes OHLC and timestamp; trade volume is also included
-request2 = req.get('https://finnhub.io/api/v1/stock/candle?symbol=AAPL&resolution=1&from=1605543327&to=1605629727&token=c0grqpv48v6ttm1smfqg')
+request2 = req.get('https://finnhub.io/api/v1/stock/candle?symbol=AAPL&resolution=1&from=1605543327&to=1605629727&token=sandbox_c0grqpv48v6ttm1smfqg')
 aapl_candle = request2.json()
 print(aapl_candle)
 
-
-
-
-
+crypto = req.get('https://finnhub.io/api/v1/crypto/candle?symbol=BINANCE:BTCUSDT&resolution=D&from=1572651390&to=1575243390&token=sandbox_c0grqpv48v6ttm1smfr0')
+print(r.json())
