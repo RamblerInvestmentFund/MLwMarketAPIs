dc3116512afca99e624cf7ca27255d94976322f0
anthonypeters <apeters10@luc.edu>
2021-04-12 23:10:00 -0500

    Edited to pull daily closing price over 3 months

--- Pull API/TDA_YFIN_CSV/TDA_CSV.py
+++ Pull API/TDA_YFIN_CSV/TDA_CSV.py
@@ -3,34 +3,41 @@ import json
 import csv
 import pandas as pd
 
-#### Retrieve Ticker Option Chain
-def retrieveTickerOptionChain(ticker):
-    endpoint = endpoint = 'https://api.tdameritrade.com/v1/marketdata/chains?&symbol={stock_ticker}'
+#### Create Empty CSV for data Transfer
+def create_csv(tickers):
+    df = pd.DataFrame()
+    for i in tickers:
+        written = df.to_csv(path_or_buf="/Users/anthonypeters/Desktop/Coding-Jobs-and-Projects/For-Projects/Projects-Python/MLwMarketAPIs/Pull API/TDA_YFIN_CSV/TDA_CSV/price_data_{ticker}.csv".format(ticker = i))
+    return(written)
+####
+
+#### Retrieve Ticker Price History
+def retrieveTickerPriceHistory(ticker):
+    endpoint = 'https://api.tdameritrade.com/v1/marketdata/{stock_ticker}/pricehistory'
     full_url = endpoint.format(stock_ticker=ticker)
-    page = requests.get(url=full_url,
-                        params={'apikey' : 'NXHAYFIAP0TXGN9NFR27DDAF7QSNQPHA'})
+    parameters = {'apikey' : 'NXHAYFIAP0TXGN9NFR27DDAF7QSNQPHA',
+                        'periodType': 'month',
+                        'period': 3,
+                        'frequencyType': 'daily',
+                        'frequency': 1}
+    page = requests.get(url=full_url, params=parameters)
     content = json.loads(page.content)
     return content
 ####
 
-#### Create Empty CSV for data Transfer
-def create_csv(tickers):
-    df = pd.DataFrame(data=None)
-    for i in tickers:
-        written = df.to_csv(path_or_buf="/Users/anthonypeters/Desktop/Coding-Jobs-and-Projects/For-Projects/Projects-Python/MLwMarketAPIs/Pull API/TDA_YFIN_CSV/TDA_CSV/option_data_{ticker}.csv".format(ticker = i))
-    return(written)
-####
-
 #### Pull data and write to CSV
 def pull_close(tickers):
+    df = pd.DataFrame()
+
     for i in tickers:
-        data = retrieveTickerOptionChain(i)
-        with open('/Users/anthonypeters/Desktop/Coding-Jobs-and-Projects/For-Projects/Projects-Python/MLwMarketAPIs/Pull API/TDA_YFIN_CSV/TDA_CSV/option_data_{ticker}.csv'.format(ticker = i), 'w') as csv_file:
-            writer = csv.writer(csv_file)
-            for key, value in data.items():
-                writer.writerow([key, value])
+        output = retrieveTickerPriceHistory(i)
+        for j in output["candles"]:
+            df = df.append(j, ignore_index=True)
+        written = df.to_csv(path_or_buf="/Users/anthonypeters/Desktop/Coding-Jobs-and-Projects/For-Projects/Projects-Python/MLwMarketAPIs/Pull API/TDA_YFIN_CSV/TDA_CSV/price_data_{ticker}.csv".format(ticker = i))
 ####
 
-tickers = ["AAPL", "AMZN", "MSFT", "JNJ"]
+tickers = ["TSLA"]
 create_csv(tickers)
+pull_close(tickers)
+#print(retrieveTickerPriceHistory(ticker))
 pull_close(tickers)
--- Pull API/TDA_YFIN_CSV/YFIN_CSV.py
+++ Pull API/TDA_YFIN_CSV/YFIN_CSV.py
@@ -4,13 +4,10 @@ import csv
 def pull_close(tickers):
     #### Pulls and cleans data into priceDict from yfinance
     for i in tickers:
-        data = yf.download(tickers=i, group_by="Close", interval="1d")
-        priceList = data['Close']
-        written = priceList.to_csv(path_or_buf="stock_data_{ticker}.csv".format(ticker = i))
+        data = yf.download(tickers=i, group_by="Close", period="3mo", interval="1d")
+        price = data['Close']
+        written = price.to_csv(path_or_buf="stock_data_{ticker}.csv".format(ticker = i))
     return(written)
 
-tickers = ["AAPL", "AMZN", "MSFT", "JNJ"]
+tickers = ["TSLA"]
 pull_close(tickers)
-# with open('stock_data.csv', mode='w') as stock_data:
-#     stock_data = csv.writer(stock_data)
-#     stock_data.writerows(pull_close(tickers))
